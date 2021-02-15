b742b626b8d3e458f4279c6b799c7c3e490b12e0
ardelianvlad <ardelyan.vladyslav@gmail.com>
2021-02-15 15:56:37 +0200

    Mega fix.

--- main.py
+++ main.py
@@ -116,9 +116,14 @@ def choose(message):
 
 
 @app.route("/bot", methods=['POST'])
-def get_message():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
 
 
 @app.route("/")
